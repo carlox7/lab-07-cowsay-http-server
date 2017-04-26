5cab8b7db5ffea39fdb82df6d0fa987ffd14f370
Carlo Santos <carlox7@gmail.com>
2017-04-26 10:45:41 -0700

    finished GET

---
+++ lab-carlo/test/server-test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const server = require('../server');
+const cowsay = require('cowsay');
+const chai = require('chai');
+const http = require('chai-http');
+const expect = chai.expect;
+
+describe('Server module', function(){
+  before(done => {
+    server.listen(3000);
+    done();
+  });
+
+  describe('POST method', function(){
+    describe('/ endpoint', function(){
+      it('should respond with a 400 on bad request', done => {
+        chai.request(server)
+        .post('/monkeysay')
+        .send({})
+        .end(function(res){
+          expect(res.status).to.equal(400);
+        });
+        done();
+      });
+    });
+    describe('GET method', function(){
+      describe('/ endpoint', function(){
+        it('should respond with a 400 on bad request', done => {
+
+          done();
+        });
+      });
+    });
+  });
+});
